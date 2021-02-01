718e615cd93e936fea0a878aad6ece7d1a52b643
Love98729 <lovepreetkkaur1998@gmail.com>
2021-02-02 02:51:38 +0530

    Initial Commit

---
+++ A2_FA_IOS_LOVEPREET_C0790030/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_FA_IOS_LOVEPREET_C0790030
+//
+//  Created by user189760 on 2/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
