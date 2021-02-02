f68625e38cbc61173f36ced02134b0e2108ec448
Love98729 <lovepreetkkaur1998@gmail.com>
2021-02-02 19:32:42 +0530

    Final Assignment Done

--- A2_FA_IOS_LOVEPREET_C0790030/ViewController.swift
+++ A2_FA_IOS_LOVEPREET_C0790030/ViewController.swift
@@ -107,15 +107,4 @@ class Add_Update_ProductVC: UIViewController {
         }
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
